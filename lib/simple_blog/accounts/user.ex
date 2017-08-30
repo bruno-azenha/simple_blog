91bce39699d1624d96b1cec7e6c9a528b6a9b6f5
Bruno Azenha Gonçalves <azenha.bruno@gmail.com>
2017-08-29 22:10:58 -0300

    Adds email credential to users

--- lib/simple_blog/accounts/user.ex
+++ lib/simple_blog/accounts/user.ex
@@ -1,12 +1,13 @@
 defmodule SimpleBlog.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
-  alias SimpleBlog.Accounts.User
+  alias SimpleBlog.Accounts.{User, Credential}
 
 
   schema "users" do
     field :name, :string
     field :username, :string
+    has_one :credential, Credential
 
     timestamps()
   end
